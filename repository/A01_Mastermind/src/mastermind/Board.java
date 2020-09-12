932c62fdebaef84d68b24a787e4002597e0f89b6
Trevor C <colto@DESKTOP-90KAFIJ>
2020-09-12 16:34:40 -0600

    Changed files and class layouts

--- repository/A01_Mastermind/src/mastermind/Board.java
+++ repository/A01_Mastermind/src/mastermind/Board.java
@@ -1,53 +1,207 @@
 package mastermind;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.JPanel;
+
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
 public class Board extends JPanel {
 	
-	private int turn = 0;
+	private int turn = 9;
 	private int guess = 0;
-	private BallColors[] row;
-	private PegColors[] guessRow;
-	private BallColors[] secretCode;
-	private int MAX_TRIES;
 	private int rows = 10;
 	private int coloumns = 4;
-	private Color color = Color.blue;
+	
+	private JButton btnRed;
+	private JButton btnYellow;
+	private JButton btnBlue;
+	private JButton btnGreen;
+	private JButton btnWhite;
+	private JButton btnBlack;
+	private JButton btnCheck;
+	private JButton btnClear;
+	
 
 	/**
 	 * Create the panel.
 	 */
 	public Board() {
+		setBackground(Color.LIGHT_GRAY);
 		
-	}
-	
-	public void changeColor(Color c) {
-		this.color = c;
+		createRedBtn();
+		setLayout(null);
+		add(btnRed);
+		
+		createYellowBtn();
+		add(btnYellow);
+		
+		createBlueBtn();
+		add(btnBlue);
+		
+		createGreenBtn();
+		add(btnGreen);
+		
+		createWhiteBtn();
+		add(btnWhite);
+		
+		createBlackBtn();
+		add(btnBlack);
+		
+		createCheckBtn();
+		add(btnCheck);
+		
+		createClearBtn();
+		add(btnClear);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(color);
+		g.setColor(Color.GRAY);
 		for (int y = 0; y < rows; y++) {
 			for (int x = 0; x < coloumns; x++) {
 				g.drawOval((x + 1) * 60, (y + 1) * 60, 40, 40);
 				g.fillOval((x + 1) * 60, (y + 1) * 60, 40, 40);
 			}
-			for (int i = 0; i < 2; i++) {
-				g.drawOval((coloumns + 1) * 60, (y + 1) *60, 10, 10);
-				g.fillOval((coloumns + 1) * 60, (y + 1) *60, 10, 10);
-				g.drawOval((coloumns + 1) * 64, (y + 1) *60, 10, 10);
-				g.fillOval((coloumns + 1) * 64, (y + 1) *60, 10, 10);
-				
-				g.drawOval((coloumns + 1) * 60, (y+1) * 30, 10, 10);
-				g.fillOval((coloumns + 1) * 60, (y+1) * 30, 10, 10);
-				g.drawOval((coloumns + 1) * 64, (y+1) * 30, 10, 10);
-				g.fillOval((coloumns + 1) * 64, (y+1) * 30, 10, 10);
-			}
 		}
+		for (int x =0; x < coloumns; x++) {
+			g.drawRect((x+1)*60, 3, 40, 40);
+			g.fillRect((x+1)*60, 3, 40, 40);
+			
+			g.drawOval((x+1)*60, 3, 40, 40);
+			g.fillOval((x+1)*60, 3, 40, 40);
+		}
+	}
+	
+	private void createClearBtn() {
+		btnClear = new JButton("Clear");
+		btnClear.setBounds(226, 755, 80, 30);
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				guess = 0;
+				Graphics g = getGraphics();
+				g.setColor(Color.gray);
+				for (int x = 0; x < coloumns; x++) {
+					g.drawOval((x + 1) * 60, (turn + 1) * 60, 40, 40);
+					g.fillOval((x + 1) * 60, (turn + 1) * 60, 40, 40);
+				}
+			}
+		});
+		btnClear.setMinimumSize(new Dimension(80, 30));
+		btnClear.setDefaultCapable(false);
+	}
+	private void createCheckBtn() {
+		btnCheck = new JButton("Check");
+		btnCheck.setBounds(46, 755, 80, 30);
+		btnCheck.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(guess > 3 && turn > 0) {
+					turn--;
+					guess = 0;
+				}
+				if(turn == 0) {
+					
+				}
+			}
+		});
+		btnCheck.setMinimumSize(new Dimension(80, 30));
+	}
+	private void createBlackBtn() {
+		btnBlack = new JButton("");
+		btnBlack.setBounds(136, 678, 80, 30);
+		btnBlack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (guess < 4) {
+					paintCircle(Color.BLACK);
+					guess++;
+					}
+			}
+		});
+		btnBlack.setMinimumSize(new Dimension(80, 30));
+		btnBlack.setBackground(Color.BLACK);
+	}
+	private void createWhiteBtn() {
+		btnWhite = new JButton("");
+		btnWhite.setBounds(226, 678, 80, 30);
+		btnWhite.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (guess < 4) {
+					paintCircle(Color.WHITE);
+					guess++;
+					}
+			}
+		});
+		btnWhite.setMinimumSize(new Dimension(80, 30));
+		btnWhite.setBackground(Color.WHITE);
+	}
+	private void createGreenBtn() {
+		btnGreen = new JButton("");
+		btnGreen.setBounds(46, 678, 80, 30);
+		btnGreen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (guess < 4) {
+					paintCircle(Color.GREEN);
+					guess++;
+					}
+			}
+		});
+		btnGreen.setMinimumSize(new Dimension(80, 30));
+		btnGreen.setBackground(Color.GREEN);
+	}
+	private void createBlueBtn() {
+		btnBlue = new JButton("");
+		btnBlue.setBounds(226, 714, 80, 30);
+		btnBlue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (guess < 4) {
+					paintCircle(Color.BLUE);
+					guess++;
+					}
+			}
+		});
+		btnBlue.setMinimumSize(new Dimension(80, 30));
+		btnBlue.setBackground(Color.BLUE);
+	}
+	private void createYellowBtn() {
+		btnYellow = new JButton("");
+		btnYellow.setBounds(136, 714, 80, 30);
+		btnYellow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (guess < 4) {
+					paintCircle(Color.YELLOW);
+					guess++;
+					}
+
+			}
+		});
+		btnYellow.setMinimumSize(new Dimension(80, 30));
+		btnYellow.setBackground(Color.YELLOW);
+	}
+	private void createRedBtn() {
+		btnRed = new JButton("");
+		btnRed.setBounds(46, 714, 80, 30);
+		
+		btnRed.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (guess < 4) {
+					paintCircle(Color.RED);
+					guess++;
+					}
+			}
+		});
+		btnRed.setMinimumSize(new Dimension(80, 30));
+		btnRed.setBackground(Color.RED);
+	}
+	private void paintCircle(Color c) {
+		Graphics g = getGraphics();
+		g.setColor(c);
+		g.drawOval((guess+1) * 60, (turn+1) * 60, 40, 40);
+		g.fillOval((guess+1) * 60, (turn+1) * 60, 40, 40);
 	}
 }
--- repository/A01_Mastermind/src/mastermind/MainWindow.java
+++ repository/A01_Mastermind/src/mastermind/MainWindow.java
@@ -3,10 +3,13 @@ package mastermind;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import net.miginfocom.swing.MigLayout;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame {
@@ -31,41 +34,66 @@ public class MainWindow extends JFrame {
 	 */
 	public MainWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 911, 833);
-		getContentPane().setLayout(new MigLayout("", "[375.00,grow][548.00,grow]", "[557.00,grow][96.00,grow]"));
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		getContentPane().add(panel, "cell 0 0,grow");
+		setBounds(100, 100, 834, 880);
+		getContentPane().setLayout(null);
 		
 		JPanel displayBoard = createBoard();
-		getContentPane().add(displayBoard, "cell 1 0 1 2,grow");
+		getContentPane().add(displayBoard);
 		
-		JPanel displayControls = createControlPanel();
-		getContentPane().add(displayControls, "cell 0 1,grow");
+		JPanel titlePanel = createTitlePanel();
+		titlePanel.setLayout(null);
+		
+		JLabel lblTitle = createTitleLabel();
+		titlePanel.add(lblTitle);
+		
+		JTextArea txtrRulesWillGo = createRulesTextPanel();
+		titlePanel.add(txtrRulesWillGo);
+		
+		JPanel outputPanel = createOutputPanel();
+		getContentPane().add(outputPanel);
 	}
 
-	private JPanel createControlPanel() {
-		JPanel displayControls = new JPanel();
-		
-		displayControls.setLayout(new BorderLayout(0, 0));
-		displayControls.setBackground(Color.LIGHT_GRAY);
-		
-		PlayerControls controlPanel = new PlayerControls();
-		displayControls.add(controlPanel);
-		return displayControls;
+	private JPanel createTitlePanel() {
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBounds(413, 11, 395, 609);
+		getContentPane().add(titlePanel);
+		return titlePanel;
+	}
+
+	private JLabel createTitleLabel() {
+		JLabel lblTitle = new JLabel("MASTERMIND");
+		lblTitle.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 35));
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setBounds(10, 11, 375, 61);
+		return lblTitle;
+	}
+
+	private JTextArea createRulesTextPanel() {
+		JTextArea txtRules = new JTextArea();
+		txtRules.setFont(new Font("Monospaced", Font.PLAIN, 11));
+		txtRules.setEditable(false);
+		txtRules.setText("Object of the game:\r\nThe computer has selected a secret combination \r\nof 4 colored pegs and you have to guess that \r\ncombination in 10 or fewer tries to win.\r\n\r\nHow to play:\r\nTo create your guess click each apparent \r\ncolored peg until you've filled the row with \r\nyour combination (you can delete your guess\r\nby clicking \"clear\" button). Then click the \r\n\"check\" button.\r\n\r\nEach time you submit a guess the machine will \r\nuse score pegs to let you know how close that\r\nguess is.\r\n\r\nFor each guess a black score peg indicates that \r\none of your pegs is the right color in the right \r\nposition. A white score peg indicates that one \r\nof your pegs is the right color in the wrong\r\nposition.\r\n\r\nUse the score to guide your next guess. \r\nIf your guess scores 4 black score pegs within \r\n10 tries, you win.\r\n\r\nTo begin a new game click the \"new game\" button.");
+		txtRules.setBounds(10, 147, 375, 439);
+		return txtRules;
+	}
+
+	private JPanel createOutputPanel() {
+		JPanel outputPanel = new JPanel();
+		outputPanel.setBackground(Color.DARK_GRAY);
+		outputPanel.setBounds(413, 631, 395, 201);
+		return outputPanel;
 	}
 
 	private JPanel createBoard() {
 		JPanel displayBoard = new JPanel();
+		displayBoard.setBounds(10, 11, 393, 821);
 		
 		displayBoard.setLayout(new BorderLayout(0, 0));
-		displayBoard.setBackground(Color.LIGHT_GRAY);
+		displayBoard.setBackground(Color.GRAY);
 		
 		Board board = new Board();
-		displayBoard.add(board);
+		displayBoard.add(board, BorderLayout.CENTER);
 		
 		return displayBoard;
 	}
-
 }
