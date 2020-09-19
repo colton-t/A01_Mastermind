27455fe67aeed9a8bb006c29ff3f8504f22a1436
Austin F <Austin.Fashimpaur@gmail.com>
2020-09-18 21:37:02 -0600

    updated lbl square bounds

--- repository/A01_Mastermind/src/mastermind/Board.java
+++ repository/A01_Mastermind/src/mastermind/Board.java
@@ -101,7 +101,7 @@ public class Board extends JPanel {
 		for(int i = 0; i <= 3; i++) {
 			lblNewLabel = new JLabel(" ");
 			lblNewLabel.setOpaque(true);
-			lblNewLabel.setBounds((guess+1) * 20 + xAdjustment, (turn+1) * 52 + yAdjustment, 10, 10);
+			lblNewLabel.setBounds((guess+1) * 20 + xAdjustment, (turn+1) * 45 + yAdjustment + 62, 10, 10);
 			
 			if(blackPegs > 0) { //set color of squares to black for black pegs
 				lblNewLabel.setBackground(Color.black);
